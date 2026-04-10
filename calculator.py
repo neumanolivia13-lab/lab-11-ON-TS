52c21a9bc20facac434b8f1ba97fc87b3a1a5d41
neumanolivia13-lab <neumanolivia13@gmail.com>
2026-04-10 14:34:14 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,28 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
-
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+def add(a, b):
+    return a + b
 
 
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+
+
+def logarithm(x, base):
+    if x <= 0:
+        raise ValueError("Invalid argument")
+    if base <= 0 or base == 1:
+        raise ValueError("Invalid base")
+    return math.log(x, base)
+
+
+def exp(x):
+    return math.exp(x)
